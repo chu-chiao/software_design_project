56b46204108f5e7385087d2a53feb77a66b28af6
JoannaEbreso <jebreso@yahoo.com>
2022-11-19 18:23:22 +0000

    changes in authentication class

--- src/main/java/com/demovehiclepro/util/AuthServiceFactory.java
+++ src/main/java/com/demovehiclepro/util/AuthServiceFactory.java
@@ -14,6 +14,7 @@ public final class AuthServiceFactory {
     @Autowired
     DealerAuthService dealerAuthService;
 
+    @Autowired
     SalesExecutiveAuthService salesExecutiveAuthService;
 
     public AuthService getAuthService(RegistrationDTO registrationDTO){
